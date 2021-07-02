5b631b25c5c455954a7cc9b62966be2a42deb80f
Maia <maiafleschler@gmail.com>
2021-07-02 18:04:12 -0300

    Agrega informacion

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+    $nombre = "Maia";
+    $apellido = "Fleschler";
+    $edad = 30;
+    $hobbie = "Pintar";
+    $editor = "Visual Studio Code";
+    $sistemaOp = "Windows";
+
+    echo "Nombre: ".$nombre."<br>Apellido: ".$apellido."<br>Edad: ".$edad."<br>Hobbie: ".$hobbie."<br>Editor de código preferido: ".$editor."<br>Sistema operativo que utiliza: ".$sistemaOp;
+
+?>
